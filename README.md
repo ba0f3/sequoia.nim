45d839c8f658680a959e762dd36845b2ed81c704
root <me@huy.im>
2018-10-12 09:52:01 +0700

    32bit and  dynamic link supports

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # sequoia.nim
 Sequoia PGP wrapper for Nim https://gitlab.com/sequoia-pgp
+        
+
+## Dependences
+- nettle-dev
+- libsqilte3
+- libssl
+- libcrypto
+- libpthread
+
+## Building
+
+By default, this module uses static linked libraies provided in src/sequioa/lib, you can use dynamic linked with `SQ_DYMANIC_LINK` swtich
 
--- src/sequoia.nim
+++ src/sequoia.nim
@@ -2,8 +2,15 @@
 # Copyright Huy Doan
 # Sequoia PGP wrapper for Nim
 import ospaths
-const LIB_DIR = currentSourcePath().splitPath.head & "/sequoia/lib"
-{.passL: "-L" & LIB_DIR & " -lsequoia_ffi -lssl -lcrypto -lnettle -lhogweed -lgmp -lsqlite3 -lpthread".}
+when defined(SQ_DYNAMIC_LINK):
+  {.passL: "-lsequoia_ffi -lssl -lcrypto -lnettle -lhogweed -lgmp -lsqlite3 -lpthread".}
+else:
+  const LIB_DIR = currentSourcePath().splitPath.head & "/sequoia/lib"
+  when hostCPU == "i386":
+    const SUFFIX = "32"
+  else:
+    const SUFFIX = "64"
+  {.passL: "-L" & LIB_DIR & " -lsequoia_ffi" & SUFFIX & " -lssl -lcrypto -lnettle -lhogweed -lgmp -lsqlite3 -lpthread".}
 
 {.pragma: sequioa, cdecl, importc.}
 
